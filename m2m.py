14bbc0ce27c7d2352edf2a77435a975930f47fc8
Cmrd-Dan <94006190+Cmrd-Dan@users.noreply.github.com>
2023-04-02 14:00:11 -0400

    removed fluff

--- m2m.py
+++ m2m.py
@@ -1,20 +1,14 @@
 def Converter():
-    import time
-    print("Please enter how many lines of code you want to convert.")
     lines = input()
-    pcmax = int(lines)*4
-    print(f"You want to convert {lines} lines, and your program counter is from 0 to {pcmax}\n")
-    time.sleep(2)
-    print(f"Please enter your {lines} lines of Mips code")
-    print("please enter the register number, EX: $4 = $a0, $0 = $zero")
-    count = 0
+    pcmax = int(lines) * 4
     code = []
-    while count < int(lines):
-        temp = input()
-        code.append(temp)
-        count = count + 1
-    print(code)
+    while len(code) < int(lines):
+        code.append(input())
+    print(code,pcmax)
     return
     pass
 
 def codechecker(code):
+    pass
+
+Converter()
